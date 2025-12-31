e849145c9b0272f744e498747c3d7bb77b53e178
Oguzhan Yildirim <yildirimogcod@gmail.com>
2025-12-31 13:26:06 +0300

    initial: project skeleton with dependencies

---
+++ src/main/java/org/yildirimog/flightturk/FlightTurkApplication.java
@@ -0,0 +1,13 @@
+package org.yildirimog.flightturk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightTurkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightTurkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/yildirimog/flightturk/FlightTurkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yildirimog.flightturk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightTurkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
